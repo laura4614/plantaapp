906218d7b695f09b17b9f464bb4f868d290b2355
laura <lauravargas4614@gmail.com>
2020-05-28 09:23:12 -0500

    Updates

---
+++ precache-manifest.44d2b2f72bad3964b13444e43739c095.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3ea7dc01bf65f2fc1a5b2a553f0c380",
+    "url": "/laura4614/plantaapp.git/index.html"
+  },
+  {
+    "revision": "13551d682f425aac2a91",
+    "url": "/laura4614/plantaapp.git/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f68542d1bbdee406971e",
+    "url": "/laura4614/plantaapp.git/static/css/main.6b4532cf.chunk.css"
+  },
+  {
+    "revision": "13551d682f425aac2a91",
+    "url": "/laura4614/plantaapp.git/static/js/2.e150fbdd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/laura4614/plantaapp.git/static/js/2.e150fbdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f68542d1bbdee406971e",
+    "url": "/laura4614/plantaapp.git/static/js/main.53168436.chunk.js"
+  },
+  {
+    "revision": "f8b24f22e483f151499b",
+    "url": "/laura4614/plantaapp.git/static/js/runtime-main.99854fa3.js"
+  },
+  {
+    "revision": "ba631b638722bb02c7a4ee1998ba6224",
+    "url": "/laura4614/plantaapp.git/static/media/banner_img.ba631b63.jpg"
+  },
+  {
+    "revision": "0d3d721254ae1141a1533f413fcd768c",
+    "url": "/laura4614/plantaapp.git/static/media/logo.0d3d7212.png"
+  }
+]);
